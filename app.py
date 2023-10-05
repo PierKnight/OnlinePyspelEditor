ca51c75d1f69a136483b7fc91c84d52ff888ecbf
PierKnight <pier.altimari@libero.it>
2023-10-05 12:43:01 +0200

    Code Cleanup

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from time import sleep
-from flask import Flask,request
+from flask import Flask,request,jsonify
+from pyspel import pyspel
+from flask_cors import CORS
+import subprocess
 app = Flask(__name__)
 
-@app.route('api/v1/pyspel', methods=['POST'])
+CORS(app)
+
+@app.route('/api/v1/pyspel', methods=['POST'])
 def data():
 
     content_type = request.headers.get('Content-Type')
     if content_type == 'application/json':
         json = request.json
-        message = json["message"]
-        exec(message)
+        message = json['code']
+        print(message)
+        result = subprocess.run(["c:/modding/flask/venv/Scripts/python.exe","-c",message], capture_output=True,text=True)
 
-    return "e"
+        print(result.stdout)
+        return jsonify({
+            "result": result.stdout,
+            "error": result.stderr
+        })
+    return "no"
 
 if __name__ == '__main__':
     app.run()
